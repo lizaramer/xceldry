1164b41957d264ed80cd9ecac4aa5a7f948d6230
LeNav23 <A01286259@tec.mx>
2026-04-23 13:07:02 -0600

    Config dominio custom

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { inspectAttr } from 'kimi-plugin-inspect-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/xceldry/',
+  base: '/',
   plugins: [inspectAttr(), react()],
   build: {
     chunkSizeWarningLimit: 700,
